f82813c76e9650af01990303f3151c74008bcbd1
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-12-06 16:55:39 -0300

    feat: create participant component

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,47 @@
+import { 
+  Text, 
+  TextInput, 
+  View, 
+  TouchableOpacity, 
+} from 'react-native';
+
+import { Participant } from '../../components/Participant';
+
+import { styles } from './styles';
+
+export function Home() {
+  function handleParticipantAdd() {
+    console.log('Você clicou no botão adicionar participante');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+      <Text style={styles.eventDate}>
+        Quinta, 1 de dezembro de 2022
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={handleParticipantAdd}
+        >
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant name='Taka' />
+      <Participant name='Naka'/>
+    </View>
+  )
+}
