34b98df3c7bcad0f65c1d520ce9bab1d078cc4b7
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-12-06 22:54:20 -0300

    feat: remove participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -32,7 +32,7 @@ export function Home() {
       [
         {
           text: 'Sim',
-          onPress: () => Alert.alert('Deletado!'),
+          onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name)),
         },
         {
           text: 'Não',
